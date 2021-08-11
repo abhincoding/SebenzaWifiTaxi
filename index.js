3aeff69b1a963f3961db7e6687be4a19476c78d4
abhincoding <82216005+abhincoding@users.noreply.github.com>
2021-08-11 11:04:06 +0200

    changing db production v4

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(express.json()); 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"client/build")));
+    app.use(express.static(path.join(__dirname,"frontend/build")));
 }
 
 //Routes
 console.log(__dirname);
-console.log(path.join(__dirname,"client/build"));
+console.log(path.join(__dirname,"frontend/build"));
 
 
 app.post("/taxi",async(req, res) =>{
